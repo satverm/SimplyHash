af5e1405ba524b73a7c11eaf142cca20be04bb15
satverm <64876484+satverm@users.noreply.github.com>
2022-02-14 06:59:03 +0530

    Update gethash.py
    Updated instructions

--- gethash.py
+++ gethash.py
@@ -3,4 +3,5 @@ hashinput= input("Enter the input string to get hash 265 :\n")
 hashvalue= hs.sha256(hashinput.encode('utf-8')).hexdigest()
 print("The sha256 of {} is {}".format(hashinput,hashvalue))
 print(len(hashvalue), (hashvalue.encode('utf-8')))
+print('You can store or write downthe first 3 to 5 characters of the {} for confirmation later.'.format(hashvalue))
 
