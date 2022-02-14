2b5904d3792b2f47d089a4bc6dd4e1eb48146866
satverm <64876484+satverm@users.noreply.github.com>
2022-02-14 07:06:04 +0530

    Update gethash.py
    Removed len print

--- gethash.py
+++ gethash.py
@@ -2,6 +2,6 @@ import hashlib as hs
 hashinput= input("Enter the input string to get hash 265 :\n")
 hashvalue= hs.sha256(hashinput.encode('utf-8')).hexdigest()
 print("The sha256 of {} is {}".format(hashinput,hashvalue))
-print(len(hashvalue), (hashvalue.encode('utf-8')))
-print('You can store or write downthe first 3 to 5 characters of the {} for confirmation later.'.format(hashvalue))
+
+print('You can store or write down the first 3 to 5 characters of the {} for confirmation later.'.format(hashvalue))
 
