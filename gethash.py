0d310ebcad1f4b51de230ef162687747e3afbb73
satverm <64876484+satverm@users.noreply.github.com>
2022-06-24 08:15:24 +0530

    Updated for saltedhash
    Option to get a salted hash for improved security

--- gethash.py
+++ gethash.py
@@ -1,7 +1,16 @@
 import hashlib as hs
-hashinput= input("Enter the input string to get hash 265 :\n")
+hashinput= input("Enter the input string to get hash 256 :\n")
+salt= input("Enter the additional phrase which will beused as a salt:")
+saltedinput = hashinput + salt
+
 hashvalue= hs.sha256(hashinput.encode('utf-8')).hexdigest()
 print("The sha256 of {} is {}".format(hashinput,hashvalue))
 
-print('You can store or write down the first 3 to 5 characters of the {} for confirmation later.'.format(hashvalue))
+saltedhashvalue= hs.sha256(saltedinput.encode('utf-8')).hexdigest()
+print("The sha256 of {} is {}".format(saltedinput,saltedhashvalue))
+
+
+
+
+print('You can store or write down the first 3 to 5 characters of the {} and {} for confirmation later.'.format(hashvalue,saltedhashvalue))
 
