6c8f27815c1eaf84601c6aed3eeea8f57e03d524
satverm <64876484+satverm@users.noreply.github.com>
2022-06-24 21:07:30 +0530

    Update gethash.py

--- gethash.py
+++ gethash.py
@@ -1,13 +1,13 @@
 import hashlib as hs
 hashinput= input("Enter the input string to get hash 256 :\n")
-salt= input("Enter the additional phrase which will beused as a salt:")
+salt= input("Enter the additional phrase which will be used as a salt:")
 saltedinput = hashinput + salt
 
 hashvalue= hs.sha256(hashinput.encode('utf-8')).hexdigest()
 print("The sha256 of {} is {}".format(hashinput,hashvalue))
 
 saltedhashvalue= hs.sha256(saltedinput.encode('utf-8')).hexdigest()
-print("The sha256 of {} is {}".format(saltedinput,saltedhashvalue))
+print("The sha256 of saltedinput {} is {}".format(saltedinput,saltedhashvalue))
 
 
 
